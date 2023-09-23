a4d1167047c8c501d000c801fa26ecf90b48bebc
Xuradel <leagueofxuradel@gmail.com>
2023-09-23 17:21:26 -0600

    fix

--- next.config.js
+++ next.config.js
@@ -1,12 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    experimental: {
+      appDir: true,
+    },
     images: {
-        domains: [
-            "avatars.githubusercontent.com",
-            "lh3.googleusercontent.com",
-            "res.cloudinary.com",
-        ]
+      domains: [
+        'res.cloudinary.com', 
+        'avatars.githubusercontent.com',
+        'lh3.googleusercontent.com'
+      ]
     }
-}
-
-module.exports = nextConfig
+  }
+  
+  module.exports = nextConfig
